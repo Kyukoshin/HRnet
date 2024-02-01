2639b359f2167205fedbf4f886ec6494c3ad4293
RemiBertrand <rbertrand21@gmail.com>
2024-02-01 11:56:53 +0100

    Added data and fixed displaying

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,9 +1,14 @@
 import './Table.css';
 
+import { useState } from 'react';
+
 import '../../../node_modules/tabulator-tables/dist/css/tabulator.min.css';
 import { ReactTabulator } from 'react-tabulator'
 
 function Table() {
+
+    const [page, setPage] = useState(1);
+    const [size, setSize] = useState(10);
 
     const columns = [
         { title: "First Name", field: "firstName", width: 150 },
@@ -16,18 +21,54 @@ function Table() {
         { title: "State", field: "state" },
         { title: "Zip Code", field: "zipCode", hozAlign: "center" },
     ];
-    
+
     var data = [
         { id: 1, firstName: "John", lastName: "Doe", dateOfBirth: "1990-05-12", startDate: "2020-01-01", department: "Marketing", street: "123 Main Street", city: "New York", state: "NY", zipCode: "10001" },
+        { id: 2, firstName: "Jack", lastName: "Black", dateOfBirth: "1992-07-23", startDate: "2022-10-12", department: "Sales", street: "145 Second Street", city: "Chicago", state: "WN", zipCode: "25847" }
+    ];
 
-    ];    
+    const handlePageChange = (e, page) => {
+        setPage(page);
+    };
+
+    const handleSizeChange = (e, size) => {
+        setSize(size);
+    };
+
 
     return (
         <div>
             <ReactTabulator
+                key={Date.now()}
                 data={data}
                 columns={columns}
-                layout={"fitData"}
+                options={{
+                    layout: 'fitColumns',
+                    responsiveLayout: 'collapse',
+                    pagination: 'local',
+                    paginationSize: 10,
+                    paginationSizeSelector: [10, 25, 50, 100],
+                    paginationCounter: 'rows',
+                    langs: {
+                        default: {
+                            pagination: {
+                                counter: {
+                                    showing: 'Showing',
+                                    of: 'of',
+                                    rows: 'entries',
+                                    pages: 'pages',
+                                },
+                            },
+                        },
+                    },
+                }}
+                page={page}
+                size={size}
+                total={data.length}
+                onPageChange={handlePageChange}
+                onSizeChange={handleSizeChange}
+
+
             />
         </div>
     );

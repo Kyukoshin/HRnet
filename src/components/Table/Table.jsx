3c669c3ee76aac4b22abd30d3a92480ddd02aee1
RemiBertrand <rbertrand21@gmail.com>
2024-02-26 12:12:26 +0100

    Added more users and a search field to table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -11,17 +11,18 @@ function Table() {
 
   const [page, setPage] = useState(1);
   const [size, setSize] = useState(10);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const columns = [
     { title: "First Name", field: "firstName", width: 150 },
-    { title: "Last Name", field: "lastName", width: 150 },
-    { title: "Date of Birth", field: "dateOfBirth", hozAlign: "center" },
-    { title: "Start Date", field: "startDate", hozAlign: "center" },
-    { title: "Department", field: "department" },
-    { title: "Street", field: "street" },
-    { title: "City", field: "city" },
-    { title: "State", field: "state" },
-    { title: "Zip Code", field: "zipCode", hozAlign: "center" },
+    { title: "Last Name", field: "lastName", width: 150},
+    { title: "Date of Birth", field: "dateOfBirth", hozAlign: "center"},
+    { title: "Start Date", field: "startDate", hozAlign: "center"},
+    { title: "Department", field: "department"},
+    { title: "Street", field: "street"},
+    { title: "City", field: "city"},
+    { title: "State", field: "state"},
+    { title: "Zip Code", field: "zipCode", hozAlign: "center"},
   ];
 
   const handlePageChange = (e, page) => {
@@ -32,6 +33,10 @@ function Table() {
     setSize(size);
   };
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   const [data, setData] = useState(users);
 
   useEffect(() => {
@@ -39,14 +44,24 @@ function Table() {
     const newData = JSON.parse(JSON.stringify(users));
     setData(newData);
   }, [users]);
-
   
+  const filteredData = data.filter(item => {
+    return Object.values(item).some(value =>
+      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
       <ReactTabulator
         key={Date.now()}
-        data={data}
+        data={filteredData}
         columns={columns}
         options={{
           layout: 'fitColumns',
@@ -70,13 +85,10 @@ function Table() {
         }}
         page={page}
         size={size}
-        total={data.length}
+        total={filteredData.length}
         onPageChange={handlePageChange}
         onSizeChange={handleSizeChange}
       />
-
-      
-
     </div>
   );
 }

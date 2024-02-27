5adcaafb0baf4ea88b13486fd57ea1dfae2e566a
RemiBertrand <rbertrand21@gmail.com>
2024-02-27 10:21:27 +0100

    Changed from tabulator to table for accessibility

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,95 +1,156 @@
-import './Table.css';
+import React, { useMemo, useEffect, useState } from 'react';
+import { useTable, usePagination, useGlobalFilter, useSortBy } from 'react-table';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUsers } from '../../redux/userSlice';
-import { React, useState, useEffect } from 'react';
+
+import './Table.css';
 import '../../../node_modules/tabulator-tables/dist/css/tabulator.min.css';
-import { ReactTabulator } from 'react-tabulator';
 
 function Table() {
   const dispatch = useDispatch();
   const users = useSelector(selectUsers);
 
-  const [page, setPage] = useState(1);
-  const [size, setSize] = useState(10);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const columns = [
-    { title: "First Name", field: "firstName", width: 150 },
-    { title: "Last Name", field: "lastName", width: 150},
-    { title: "Date of Birth", field: "dateOfBirth", hozAlign: "center"},
-    { title: "Start Date", field: "startDate", hozAlign: "center"},
-    { title: "Department", field: "department"},
-    { title: "Street", field: "street"},
-    { title: "City", field: "city"},
-    { title: "State", field: "state"},
-    { title: "Zip Code", field: "zipCode", hozAlign: "center"},
-  ];
-
-  const handlePageChange = (e, page) => {
-    setPage(page);
-  };
-
-  const handleSizeChange = (e, size) => {
-    setSize(size);
-  };
-
-  const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
+  const columns = useMemo(
+    () => [
+      { Header: "First Name", accessor: "firstName", width: 150 },
+      { Header: "Last Name", accessor: "lastName", width: 150 },
+      {
+        Header: "Date of Birth",
+        accessor: "dateOfBirth",
+        Cell: ({ value }) => {
+          const date = value instanceof Date ? value.toLocaleDateString() : value;
+          return <span>{date}</span>;
+        },
+        width: 150,
+      },
+      {
+        Header: "Start Date",
+        accessor: "startDate",
+        Cell: ({ value }) => {
+          const date = value instanceof Date ? value.toLocaleDateString() : value;
+          return <span>{date}</span>;
+        },
+        width: 150,
+      },
+      { Header: "Department", accessor: "department" },
+      { Header: "Street", accessor: "street" },
+      { Header: "City", accessor: "city" },
+      { Header: "State", accessor: "state" },
+      { Header: "Zip Code", accessor: "zipCode", width: 100 },
+    ],
+    []
+  );
 
   const [data, setData] = useState(users);
 
   useEffect(() => {
-    console.log(users);
-    const newData = JSON.parse(JSON.stringify(users));
-    setData(newData);
+    setData(users);
   }, [users]);
-  
-  const filteredData = data.filter(item => {
-    return Object.values(item).some(value =>
-      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  });
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    state: { pageIndex, pageSize, globalFilter },
+    setGlobalFilter,
+    // pagination and sorting variables and functions
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 10 },
+    },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
 
   return (
     <div className='wrapTabulator'>
       <input
         type="text"
         placeholder="Search..."
-        value={searchTerm}
-        onChange={handleSearchChange}
+        value={globalFilter || ''}
+        onChange={(e) => setGlobalFilter(e.target.value)}
         className='searchTable'
       />
-      <ReactTabulator
-        key={Date.now()}
-        data={filteredData}
-        columns={columns}
-        options={{
-          layout: 'fitColumns',
-          responsiveLayout: 'collapse',
-          pagination: 'local',
-          paginationSize: 10,
-          paginationSizeSelector: [5, 10, 25, 50, 100],
-          paginationCounter: 'rows',
-          langs: {
-            default: {
-              pagination: {
-                counter: {
-                  showing: 'Showing',
-                  of: 'of',
-                  rows: 'entries',
-                  pages: 'pages',
-                },
-              },
-            },
-          },
-        }}
-        page={page}
-        size={size}
-        total={filteredData.length}
-        onPageChange={handlePageChange}
-        onSizeChange={handleSizeChange}
-      />
+      <table {...getTableProps()} className="react-table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <div>
+          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>
+        </div>
+        <span>
+          <label>Show{' '}
+            <select
+              value={pageSize}
+              onChange={(e) => {
+                setPageSize(Number(e.target.value));
+              }}
+            >
+              {[10, 20, 30, 40, 50].map((size) => (
+                <option key={size} value={size}>
+                  {size}
+                </option>
+              ))}
+            </select>{' '}
+            rows per page
+          </label>
+        </span>
+      </div>
     </div>
   );
 }
